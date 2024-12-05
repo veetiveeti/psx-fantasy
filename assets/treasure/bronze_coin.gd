868b46478b588b9a438505b844f2997e9cabb7f9
veetire <veeti.v.reponen@gmail.com>
2024-12-05 19:57:28 +0200

    Fix some mechanics

--- assets/treasure/bronze_coin.gd
+++ assets/treasure/bronze_coin.gd
@@ -3,6 +3,8 @@ signal coin_collected(value)
 
 @onready var coin_audio = $CoinSounds
 
+var collected = false
+
 var coin_sounds = [
     preload("res://sounds/coin3.wav")
 ]
@@ -11,7 +13,8 @@ func _process(delta):
     rotate_y(2.0 * delta)
     
 func _on_area_3d_body_entered(body):
-    if body.is_in_group("player"):
+    if body.is_in_group("player") and not collected:
+        collected = true
         play_coin()
         coin_collected.emit(1)  # Emit signal with coin value
         await coin_audio.finished
--- player/player.gd
+++ player/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody3D
 
-const SPEED = 4.0
+const SPEED = 4.5
 const JUMP_VELOCITY = 4.2
 const DASH_SPEED = 20.0
 const DASH_DURATION = 0.2
